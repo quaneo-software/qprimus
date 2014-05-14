ebf7a6ab4c773dc93bf4cfebd70cf11ed4feaf56
Alex Curtis <alx.curtis@gmail.com>
2014-05-14 12:58:52 +0100

    sanity tests

--- sanity_tests.js
+++ sanity_tests.js
@@ -2,9 +2,9 @@ var primus = require('primus');
 var http = require('http');
   
 var tcp = require('./transformer/tcp');
-var xmlparser = require('./parser/xml');
+var pipeParser = require('./parser/pipe');
 
-var Socket = primus.createSocket({ transformer: tcp, parser: xmlparser});
+var Socket = primus.createSocket({ transformer: tcp, parser: pipeParser});
 
 var url = "http://127.0.0.1:21100";
 
